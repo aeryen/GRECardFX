5b8f4a30a99a1e39691c8f3d05dfed419ce6c8d5
aeryen <aeryen@gmail.com>
2016-04-07 18:29:54 -0500

    Initial GUI setup, WIP

---
+++ src/com/aeryen/GRECardFX/CardManager.java
@@ -0,0 +1,7 @@
+package com.aeryen.GRECardFX;
+
+/**
+ * Created by aeryen on 4/7/2016.
+ */
+public class CardManager {
+}
--- src/com/aeryen/GRECardFX/Main.java
+++ src/com/aeryen/GRECardFX/Main.java
@@ -1,44 +1,83 @@
 package com.aeryen.GRECardFX;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.util.Arrays;
 
-public class Main extends Application {
+public class Main extends Application implements EventHandler {
+
+	public static final String INPUTPATH = "." + File.separator + "data" + File.separator + "08red" + File.separator;
+
+	GridPane listSelectionRoot = null;
+	Scene listSelectionScene = null;
+
+	public void initializeButtons() {
+		File rootFolder = new File(INPUTPATH);
+		if (!rootFolder.exists()) {
+			System.err.println("CANNOT FIND DATA FOLDER: " + rootFolder.getAbsolutePath());
+			return;
+		}
+		File[] listFiles = rootFolder.listFiles();
+		System.out.println("A total of " + listFiles.length + " list files are found.");
+
+		Arrays.sort(listFiles);
+		for (int i = 0; i < listFiles.length; i++) {
+			addButton(i, listFiles[i].getName().substring(0, 2));
+		}
+	}
+
+	static final private int BTN_PER_ROW = 5;
+
+	private void addButton(int index, String name) {
+		Button btn = new Button();
+		btn.setText(name);
+		btn.setOnAction(this);
+		int row = index / BTN_PER_ROW;
+		int column = index % BTN_PER_ROW;
+		row = row + 1;
+		listSelectionRoot.add(btn, column, row);
+	}
+
+	@Override
+	public void handle(Event event) {
+		String listName = ((Button) event.getSource()).getText();
+
+		System.out.println("selected List: " + listName);
+	}
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
 
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
+		try {
+			listSelectionRoot = FXMLLoader.load(getClass().getResource("listSelection.fxml"));
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+			listSelectionScene = new Scene(listSelectionRoot, 280, 450);
 
-        Scene scene = new Scene(root, 300, 250);
+			primaryStage.setTitle("Hello World!");
+			primaryStage.setScene(listSelectionScene);
 
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+			initializeButtons();
+			primaryStage.setResizable(false);
+
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
     }
 
-    public static final String INPUTPATH = "." + File.separator + "08red" + File.separator;
 
     public static void main(String[] args) {
         launch(args);
     }
+
+
 }
